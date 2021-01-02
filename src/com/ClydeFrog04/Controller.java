2c34fecf5bbc72eefb2d7c9f7f06e84acdaf8979
ClydeFrog04 <egan.randal@gmail.com>
2021-01-01 22:35:42 -0700

    feat(mainwindow.fxml): initial gui setup

--- src/com/ClydeFrog04/Controller.java
+++ src/com/ClydeFrog04/Controller.java
@@ -1,4 +1,19 @@
 package com.ClydeFrog04;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    //fxml components
+    @FXML
+    Label delayLabel;
+    @FXML
+    TextField setDelayField;
+    @FXML
+    Button toggleButton;
+    @FXML
+    Label clickStatus;
+
 }
