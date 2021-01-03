f32fdf875b661f9b1fecb8e644b80fc10c10e528
ClydeFrog04 <egan.randal@gmail.com>
2021-01-03 16:43:07 -0700

    fix(main.java): fix regex
    The previous regex allowed single non numeric characters, this has been fixed

--- src/com/ClydeFrog04/Main.java
+++ src/com/ClydeFrog04/Main.java
@@ -7,28 +7,63 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
 
 import java.awt.*;
 
 public class Main extends Application {
 
+    Controller controller;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
         Parent root = loader.load();
-        Controller controller = loader.getController();
+        controller = loader.getController();
 
         //set textfield to be numeric only
         controller.setDelayField.textProperty().addListener((observable, oldValue, newValue) -> {
-            if(!newValue.matches("\\d*.")) {
-                controller.setDelayField.setText(newValue.replaceAll("[^\\d.]", ""));
+            if(!newValue.matches("\\d*\\.")) {
+                controller.setDelayField.setText(newValue.replaceAll("[^\\d\\.]", ""));
             }
         });
 
         primaryStage.setTitle("OP Farms");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        Scene mainScene = new Scene(root, 500, 500);
+        mainScene.setOnKeyPressed(e -> {
+            System.out.println("Key pressed");
+            if(e.getCode() == KeyCode.F12){
+                handlef12();
+            }
+        });
+        primaryStage.setScene(mainScene);
+
+        /*
+
+
+        scene.setOnKeyPressed(e -> {
+    if (e.getCode() == KeyCode.A) {
+        System.out.println("A key was pressed");
+    }
+});
+         */
         primaryStage.show();
+    }
+
+    public void handlef12(){
+        //f12 will be used for initializing and ending auto crafting
+        System.out.println("F12 pressed");
+        if(controller.isCrafting){
+            //pause crafting service
+            //set status
+
+        }else{
+            if(controller.isClicking){//check if the auto clicker is running, if it is, then stop clicking before crafting
+                controller.clickService.pause();
+                controller.isClicking = false;
+            }
+        }
     }
 
 
